d2154be2de9ea3b26721eb4dc38a077b80023318
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-26 00:33:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ function nowServing(line) {
   } else {
     var name = line[0];
     line.shift();
-    return `Currently serving ${name}`; 
+    return `Currently serving ${name}.`; 
   }
+}
+
+function currentLine(line) {
+  var lineStr = 'The line is currently: '
+  if (line.length === 0) {
+    return "The line is currently empty";
+  } else {
+    for (var i = 0; i < line.length; i++) {
+      var name = line[i];
+
+      lineStr += `${i + 1}. ${name}, `;
+    }
+  }
+  return lineStr.slice(0, lineStr.length - 2);
 }

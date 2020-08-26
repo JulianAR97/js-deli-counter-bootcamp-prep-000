86352409f3c9dbf835c57990982c76454a6b69e7
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-26 00:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function takeANumber(line, name) {
 
   return `Welcome, ${name}. You are number ${place} in line.`;
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = line[0];
+    line.shift();
+    return name; 
+  }
+}
